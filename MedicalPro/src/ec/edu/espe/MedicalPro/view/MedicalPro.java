b4543fb6502b275b6004c920c45f98cd0073ce9a
guitarra155 <guitar01yfi@gmail.com>
2021-11-22 18:18:44 -0500

    PROJECT MEDICALPRO V1.0

---
+++ MedicalPro/src/ec/edu/espe/MedicalPro/view/MedicalPro.java
@@ -0,0 +1,17 @@
+// MEDICALPRO
+package ec.edu.espe.MedicalPro.view;
+
+/**
+ *
+ * @author jhona
+ */
+public class MedicalPro {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
