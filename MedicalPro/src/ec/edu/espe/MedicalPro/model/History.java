fe085e30d3bd036863041fb146346987b51ea6b8
guitarra155 <guitar01yfi@gmail.com>
2021-11-22 20:53:17 -0500

    PROJECT MEDICALPRO v1.01

---
+++ MedicalPro/src/ec/edu/espe/MedicalPro/model/History.java
@@ -0,0 +1,34 @@
+package ec.edu.espe.MedicalPro.model;
+
+/**
+ *
+ * @author Guitarra Jhon
+ */
+public class History {
+        private String name;
+        private int age;
+        private String diseases;
+        private String allergie;
+        private Boolean registered;
+        private Boolean operated;
+        private Boolean newRecord;
+        private Boolean intership;
+        
+    public void organice() {
+
+    }
+
+    public void revision() {
+
+    }
+
+    public void acceptance() {
+
+    }
+    public void difficulty() {
+
+    }
+    public void operated() {
+
+    }
+}
---
+++ MedicalPro/src/ec/edu/espe/MedicalPro/model/Medicine.java
@@ -0,0 +1,29 @@
+package ec.edu.espe.MedicalPro.model;
+
+/**
+ *
+ * @author Guitarra Jhon
+ */
+public class Medicine {
+        private String name;
+        private Float price;
+        private int amount;
+        private Boolean Availability;
+        private Boolean prescriptOnly;
+        
+    public void prescripion() {
+
+    }
+
+    public void sell() {
+
+    }
+
+    private void stock() {
+
+    }
+    
+    private void promotions() {
+
+    }
+}
