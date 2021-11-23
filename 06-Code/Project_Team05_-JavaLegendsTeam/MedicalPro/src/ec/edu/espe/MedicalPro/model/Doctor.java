3f2227f6038c741c114a9a66967af9af1f657f52
guitarra155 <guitar01yfi@gmail.com>
2021-11-22 21:58:43 -0500

    PROJECT MEDICAL V1.01

---
+++ 06-Code/Project_Team05_-JavaLegendsTeam/MedicalPro/src/ec/edu/espe/MedicalPro/model/Doctor.java
@@ -0,0 +1,34 @@
+/**
+ *
+ * @Daniel Lincango
+ */
+public class Doctor {
+    
+    private String name;
+    private Integer age;
+    private String  specialty;
+    private String workingDay;
+     
+    
+    public void  doStuff(){
+        
+    }
+    private void toDiagnose(){
+        
+    } 
+    private void medicate(){
+        
+    }
+    private void inform(){
+        
+    }
+    private void monitor(){
+        
+    }
+    private void requestExams(){
+        
+    }
+    private void prescriteTreatments(){
+        
+    }
+}
---
+++ 06-Code/Project_Team05_-JavaLegendsTeam/MedicalPro/src/ec/edu/espe/MedicalPro/model/SCHEDULE.java
@@ -0,0 +1,33 @@
+/**
+ *
+ * @Daniel Lincango
+ */
+public class SCHEDULE {
+
+    private String day;
+    private String month;
+    private Integer yaer;
+    private Integer time;
+
+    public void doStuff() {
+
+    }
+    private void workHours (){
+        
+    }
+    private void scheduleAppoinments(){
+        
+    }
+    private void reminder(){
+        
+    }
+    private void patientsControl(){
+        
+    }
+    private void personalControl(){
+        
+    }
+    private void addDescription(){
+        
+    }
+}
