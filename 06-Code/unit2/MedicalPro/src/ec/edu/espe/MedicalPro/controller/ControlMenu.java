fa196124ed2d90185f2c3e0f613db319801222ec
guitarra155 <guitar01yfi@gmail.com>
2022-01-05 21:11:57 -0500

    newww  data

---
+++ 06-Code/unit2/MedicalPro/src/ec/edu/espe/MedicalPro/controller/ControlMenu.java
@@ -0,0 +1,47 @@
+package ec.edu.espe.MedicalPro.controller;
+
+import ec.edu.espe.MedicalPro.view.Principal;
+import ec.edu.espe.MedicalPro.controller.Personal;
+import ec.edu.espe.MedicalPro.controller.Personal;
+import ec.edu.espe.MedicalPro.model.ModelPre;
+import ec.edu.espe.MedicalPro.model.ModelPre;
+import ec.edu.espe.MedicalPro.view.ViewMenu;
+
+/**
+ *
+ * @author Grupo 05 JavaLegends 
+ * Insuasti
+ * Guzman
+ * Guitarra
+ * Landazuri
+ * Lincango
+ */
+
+public class ControlMenu {
+    private ModelPre model;
+    private ViewMenu view;
+    public ControlMenu(ModelPre model, ViewMenu view) {
+        this.model = model;
+        this.view = view;
+        this.view.setControl(this);
+        this.view.setModel(model);
+    }
+
+    public void arrancar(Personal f) {
+        model.setF(f);
+        view.setVisible(true);
+    }
+
+    public void reabrir() {
+        view.dispose();
+        Principal.CONTROLLOGIN.init();
+    }
+
+    public void initInternos() {
+        Principal.CONTROLI.init();
+    }
+
+    private void close() {
+        view.dispose();
+    }
+}
