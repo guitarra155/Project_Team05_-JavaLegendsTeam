0cdba61cba2bae3573843f2bcb4c4aa63b5c99e5
milandazuri <teo.landa08@gmail.com>
2022-02-22 21:52:51 -0500

    Test

---
+++ 06-Code/unit3/MedicalPro/test/ec/edu/espe/MedicalPro/view/medicalProTest.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit5TestClass.java to edit this template
+ */
+package ec.edu.espe.MedicalPro.view;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ *
+ * @author Mateo Landazuri
+ */
+public class medicalProTest {
+    
+    public medicalProTest() {
+    }
+
+    /**
+     * Test of main method, of class medicalPro.
+     */
+    @Test
+    public void testMain() {
+        System.out.println("main");
+        String[] arg = null;
+        medicalPro.main(arg);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+    
+}
